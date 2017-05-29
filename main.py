5fea72ec1d34861c71d40209c7612e4a0521e345
Georgi Dikov <gvdikov@gmail.com>
2017-05-29 12:27:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from experiments.launcher import run_experiment_from_config
+
 
 
 if __name__ == '__main__':
     import pip
     pip.main(['install', 'pyyaml'])
+    from experiments.launcher import run_experiment_from_config
     run_experiment_from_config("experiments/configs/hybrid/head.yaml")
