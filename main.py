8694b4fc1ee8081df86d548bc77f6049c94cc060
Georgi Dikov <gvdikov@gmail.com>
2017-05-29 12:24:43 +0200

    try pip install for hbp missing packages

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from experiments.launcher import run_experiment_from_config
 
 
 if __name__ == '__main__':
+    import pip
+    pip.main(['install', 'pyyaml'])
     run_experiment_from_config("experiments/configs/hybrid/head.yaml")
