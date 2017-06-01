e4b0a08d4b4094483c16f257b2fe045f19c5ac18
Georgi Dikov <gvdikov@gmail.com>
2017-06-01 02:12:58 +0200

    optical flow algorithm bug fixes

--- stereovis/framed/algorithms/motion.py
+++ stereovis/framed/algorithms/motion.py
@@ -5,33 +5,27 @@ from scipy.linalg import lstsq
 class VelocityField:
     def __init__(self):
         self.delta_t = 1
-        self.neighbourhood_size = (2, 2)
+        self.neighbourhood_size = (3, 3)
         self.rejection_threshold = 0.005
         self.convergence_threshold = 1e-5
-        self.max_iter_steps = 5
+        self.max_iter_steps = 3
 
-    def fit_velocity_field(self, events, assume_sorted=False):
-
-        if not assume_sorted:
-            events = events[np.argsort(events[:, 0])]
-
-        # group events by polarity and strip the polarity column:
-        positive_polarity_indices = events[:, 3] == 0
-        stream_positive = events[positive_polarity_indices][:, :-1]
-        stream_negative = events[~positive_polarity_indices][:, :-1]
-
-        event_stream = stream_positive
-        velocities = np.zeros((event_stream.shape[0], 2))   # x, y velocity components
-        for event_id, e in enumerate(event_stream):
+    def _fit_velocity_field(self, events):
+        velocities = np.zeros((events.shape[0], 2))   # x, y velocity components
+        for event_id, e in enumerate(events):
             # find time-wise near events
-            candidate_events = event_stream[np.searchsorted(event_stream[:, 0], e[0] - self.delta_t, side='right'):
-                                            np.searchsorted(event_stream[:, 0], e[0] + self.delta_t, side='right')]
+            candidate_events = events[np.searchsorted(events[:, 0], e[0] - self.delta_t, side='right'):
+                                      np.searchsorted(events[:, 0], e[0] + self.delta_t, side='right')]
             # find space-wise near event (according to the Manhattan distance metric)
-            candidate_events = candidate_events[(np.abs(candidate_events[:, 1] - e[1]) < self.neighbourhood_size[0])
-                                                & (np.abs(candidate_events[:, 2] - e[2]) < self.neighbourhood_size[1])]
+            candidate_events = candidate_events[(np.abs(candidate_events[:, 1] - e[1]) <= self.neighbourhood_size[0])
+                                                & (np.abs(candidate_events[:, 2] - e[2]) <= self.neighbourhood_size[1])]
+
+            # perform a normalisation (min subtraction) and reverse to the initial domain at the end
+            min_txy = np.min(candidate_events, axis=0)
+            candidate_events = candidate_events - min_txy
 
             num_candidates = candidate_events.shape[0]
-            if num_candidates <= 2:
+            if num_candidates < 3:
                 # no need to fit plane in time-space if there are insufficient events
                 continue
 
@@ -42,9 +36,14 @@ class VelocityField:
             n_iter = iter(xrange(self.max_iter_steps+1))
             epsilon = np.inf
             while next(n_iter) < self.max_iter_steps and epsilon > self.convergence_threshold:
-                accepted_events = np.where(np.abs(np.dot(positions, current_best_params))
+                v1 = np.array([current_best_params[0], 1, 0])
+                v2 = np.array([current_best_params[1], 0, 1])
+                plane_normal = np.cross(v1, v2)
+                accepted_events = np.where(np.abs(np.dot(candidate_events, plane_normal))
                                            <= self.rejection_threshold)[0]
-                if accepted_events.size <= 2:
+                # accepted_events = np.where(np.abs(np.dot(positions, current_best_params) - times)
+                #                            <= self.rejection_threshold)[0]
+                if accepted_events.size < 3 or accepted_events.size == num_candidates:
                     # too few events to work with, interrupt fine-tuning and use the obtained result so far
                     break
                 new_best_params = lstsq(positions[accepted_events], times[accepted_events])[0]
@@ -52,9 +51,23 @@ class VelocityField:
                 current_best_params = new_best_params
 
             # set the amplitude and the x, y components of the velocity vector for the current event
-            velocities[event_id, :] = 1. / current_best_params[1:]
+            velocities[event_id, :] = current_best_params[:-1]
 
-        return velocities, event_stream[:, 1:3]
+        return velocities, events[:, 1:3]
+
+    def compute_optical_flow(self, events, assume_sorted=False):
+        if not assume_sorted:
+            events = events[np.argsort(events[:, 0])]
+
+        # group events by polarity and strip the polarity column:
+        positive_polarity_indices = events[:, 3] == 0
+        stream_positive = events[positive_polarity_indices][:, :-1]
+        stream_negative = events[~positive_polarity_indices][:, :-1]
+        positive_velocities, positive_positions = self._fit_velocity_field(stream_positive)
+        negative_velocities, negative_positions = self._fit_velocity_field(stream_negative)
+        return negative_velocities, negative_positions
+
+
 
 
 
@@ -75,7 +88,7 @@ if __name__ == '__main__':
                                                       ys=events['right'][:, 2],
                                                       ts=events['right'][:, 0],
                                                       zs=events['right'][:, 3],
-                                                      time_interval=50,
+                                                      time_interval=20,
                                                       pivots=[10000], non_pixel_value=-1,
                                                       return_time_indices=True)
 
@@ -83,10 +96,10 @@ if __name__ == '__main__':
     # plt.imshow(frames[0])
     # plt.show()
     print('start vflow')
-    vs, positions = vf.fit_velocity_field(events['right'][time_ind, :][0], assume_sorted=False)
-    small_arrows_ind = (np.abs(vs[:, 0]) < 1000) & (np.abs(vs[:, 1]) < 1000)
-    vs = vs[small_arrows_ind, :]
-    positions = positions[small_arrows_ind, :]
+    vs, positions = vf.compute_optical_flow(events['right'][time_ind, :][0], assume_sorted=False)
+    # small_arrows_ind = (np.abs(vs[:, 0]) < 100) & (np.abs(vs[:, 1]) < 100)
+    # vs = vs[small_arrows_ind, :]
+    # positions = positions[small_arrows_ind, :]
     plot_velocity_field({'xs': positions[:, 0], 'ys': positions[:, 1],
                          'vel_xs': vs[:, 0], 'vel_ys': vs[:, 1]}, frames[0])
 
